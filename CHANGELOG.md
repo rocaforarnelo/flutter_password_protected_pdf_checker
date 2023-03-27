695158cdf065aa418f37525767360b970d16e786
Tarique Khan <tarique.khan3071@gmail.com>
2023-03-27 12:09:59 +0530

    Implement: Add Support for iOS

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+## 1.1.0
+* Added support for iOS
+
+
 ## 1.0.0
 * Help in checking whether the PDF is password protected or not
--- ios/Classes/PasswordProtectedPdfCheckerPlugin.swift
+++ ios/Classes/PasswordProtectedPdfCheckerPlugin.swift
@@ -1,14 +1,23 @@
 import Flutter
 import UIKit
+import PDFKit
 
 public class PasswordProtectedPdfCheckerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "password_protected_pdf_checker", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "com.tarique/password_protected_pdf_checker", binaryMessenger: registrar.messenger())
     let instance = PasswordProtectedPdfCheckerPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    var isEncrypted = false
+    guard let flutterData = call.arguments as? FlutterStandardTypedData else {
+        result(isEncrypted)
+        return
+    }
+    if let pdfDocument = PDFDocument(data: flutterData.data) {
+        isEncrypted = pdfDocument.isEncrypted
+    }
+    result(isEncrypted);
   }
 }
